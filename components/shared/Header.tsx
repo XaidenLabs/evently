5322ec04120a19e743f0257088def80ed9c38a60
DADIENG ALFRED GABRIEL <133643084+SWITLYF@users.noreply.github.com>
2024-10-31 15:55:17 +0100

    logo changed

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -11,8 +11,8 @@ const Header = () => {
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
           <Image 
-            src="/assets/images/logo.svg" width={128} height={38}
-            alt="Evently logo" 
+            src="/assets/images/logo.png" width={90} height={5}
+            alt="ScreamAds logo" 
           />
         </Link>
 
